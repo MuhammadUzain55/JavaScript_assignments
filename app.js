ff92f6d8b46fc9e872eee6bdc982ee0a2f4f1f1a
MuhammadUzain55 <uzainsheikh55@gmail.com>
2025-02-20 14:23:21 +0500

    Updated app.js

--- app.js
+++ app.js
@@ -65,3 +65,34 @@
 // alert(age);
 // alert(profession);
 
+// Qno.5: Write a script to display the following alert using one JS variable:
+
+// var food = "pizza \npizz \npiz \npi \np\n";
+// alert(food);
+
+// Qno.6: Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+// var email = "uzainsheikh55@gmail.com";
+// alert("My email address is" + " " + email);
+
+// Qno.7: Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+
+// var book = "A smarter way to learn Javascript.";
+// alert("I am trying to learn from the book"+ " " + book);
+
+// Qno.8: Write a script to display this in browser through JS
+
+// document.write("<h1>Yah! I can write HTML content through JavaScript</h1>")
+
+// Qno.9: Store following string in a variable and show in alert and
+// browser through JS
+
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(design);
+// document.write(design);
+
