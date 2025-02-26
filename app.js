32ade490c6078d765bb63b08bfd9156fe958639d
MuhammadUzain55 <uzainsheikh55@gmail.com>
2025-02-26 16:04:05 +0500

    Updated app.js

--- app.js
+++ app.js
@@ -2,30 +2,30 @@
 
 // Qno.1: Write a script to greet your website visitor using JS alert box.
 
-// alert(Hello);
+// Ans: alert(Hello);
 
 // Qno.2: Write a script to display following message on your web page:
 
-// alert("Error! Please enter a valid a password.");
+// Ans: alert("Error! Please enter a valid a password.");
 
 // Qno.3: Write a script to display following message on your web page: (Hint : Use line break)
 
-// alert("Welcome to JS Land...\n Happpy Coding!");
+// Ans: alert("Welcome to JS Land...\n Happpy Coding!");
 
 // Qno.4: Write a script to display following messages in sequence:
 // 1. Welcome to JS Land..
 // 2. Happy coding!
 
-// alert(Welcome to JS Land...)
+// Ans: alert(Welcome to JS Land...)
 // alert(Happy coding!)
 
 // Qno.5: Generate the following message through browser’s developer console:
 
-// console.log("Hello... I can run JS through my web browser's console");
+// Ans: console.log("Hello... I can run JS through my web browser's console");
 
 // Qno.6: Make use of alerts in your new/existing HTML & CSS project.
 
-// alert("HTML & CSS project");
+// Ans: alert("HTML & CSS project");
 
 // Qno.7: Practice placement of <script></script> element in
 // following sections of your project in exercise 6:
@@ -40,25 +40,25 @@
 
 // Qno.1: Declare a variable called username.
 
-// var userName = "Uzain"
+// Ans: var userName = "Uzain"
 
 // Qno.2: Declare a variable called myName & assign to it a string
 // that represents your Full Name.
 
-// var myName = "Muhammad Uzain Sheikh"
+// Ans: var myName = "Muhammad Uzain Sheikh"
 
 // Qno.3: Write script to
 // a) Declare a JS variable, titled message.
 // b) Assign “Hello World” to variable message
 // c) Display the message in alert box.
 
-// var message = "Hello World";
+// Ans: var message = "Hello World";
 // alert(message);
 
 // Qno.4: Write a script to save student’s bio data in JS variables and
 // show the data in alert boxes.
 
-// var name = "Uzain Sheikh";
+// Ans: var name = "Uzain Sheikh";
 // var age = "21 Year's old";
 // var profession = "Certified Mobile Application Development";
 // alert(name);
@@ -67,7 +67,7 @@
 
 // Qno.5: Write a script to display the following alert using one JS variable:
 
-// var food = "pizza \npizz \npiz \npi \np\n";
+// Ans: var food = "pizza \npizz \npiz \npi \np\n";
 // alert(food);
 
 // Qno.6: Declare a variable called email and assign to it a string that
@@ -75,24 +75,24 @@
 // Show the blow mentioned message in an alert box.(Hint: use
 // string concatenation)
 
-// var email = "uzainsheikh55@gmail.com";
+// Ans: var email = "uzainsheikh55@gmail.com";
 // alert("My email address is" + " " + email);
 
 // Qno.7: Declare a variable called book & give it the value “A
 // smarter way to learn JavaScript”. Display the following
 // message in an alert box:
 
-// var book = "A smarter way to learn Javascript.";
+// Ans: var book = "A smarter way to learn Javascript.";
 // alert("I am trying to learn from the book"+ " " + book);
 
 // Qno.8: Write a script to display this in browser through JS
 
-// document.write("<h1>Yah! I can write HTML content through JavaScript</h1>")
+// Ans: document.write("<h1>Yah! I can write HTML content through JavaScript</h1>")
 
 // Qno.9: Store following string in a variable and show in alert and
 // browser through JS
 
-// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// Ans: var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
 // alert(design);
 // document.write(design);
 
@@ -101,7 +101,7 @@
 // Qno.1: Declare a variable called age & assign to it your age. Show
 // your age in an alert box.
 
-// var age = "I am 21 years old";
+// Ans: var age = "I am 21 years old";
 // alert(age);
 
 // Qno.2: Declare & initialize a variable to keep track of how many
@@ -109,7 +109,7 @@
 // number of visits on your web page. For example: “You
 // have visited this site N times”.
 
-// var visitCount = localStorage.getItem("visitCount");
+// Ans: var visitCount = localStorage.getItem("visitCount");
 // if (visitCount === null) {
 //     visitCount = 1;
 // } else {
@@ -121,7 +121,22 @@
 // Qno.3: Declare a variable called birthYear & assign to it your
 // birth year. Show the following message in your browser:
 
-var birthYear = 2004;
-document.write("<p>MY birth year is $birthYear</p>")
+// Ans: var birthYear = 2004;
+// document.write("<p>My birth year is" + " " + birthYear + "<br>Data type of my declared variable is number" + "</p>");
+
+// Qno.4: A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+// Ans: var visitorName = "John Doe";
+// var productTitle = "T-Shirt";
+// var quantity = 5;
+// document.write("<b>" + visitorName + "</b> ordered <b>" + quantity + "</b> <b>" + productTitle + "</b>(s) XYZ Clothing store.");
 
 
