b84669f7b650f0c035edec73cf0aae6c941fb2cc
MuhammadUzain55 <uzainsheikh55@gmail.com>
2025-03-05 15:47:09 +0500

    Updated app.JS

--- app.js
+++ app.js
@@ -234,8 +234,33 @@
 
 // Ans: 
 
-// Qno.6: 
+/* Qno.6: Write a program to take input the marks obtained in three
+subjects & total marks. Compute & show the resulting
+percentage on your page. Take percentage & compute
+grade as per following table:
 
+Ans: */
+
+/* Qno.7: Guess game:
+Store a secret number (ranging from 1 to 10) in a variable.
+Prompt user to guess the secret number.
+a. If user guesses the same number, show “Bingo! Correct
+answer”.
+b. If the guessed number +1 is the secret number, show
+“Close enough to the correct answer”.
+
+ANs: */
+
+/* Qno.8: Write a program to check whether the given number is
+divisible by 3. Show the message to the user if the number
+is divisible by 3.
+
+Ams: */
+
+/* Qno.9: Write a program that checks whether the given input is an
+even number or an odd number.
+
+Ans: */
 
 // ========================= Chapter# 12-13 (IF...ELSE & ELSE IF STATEMENT, TESTING SET OF CONDITIONS) ====================
 
