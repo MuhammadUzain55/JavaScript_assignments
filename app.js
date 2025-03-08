9a726e7068b989269703d21ba07de348cdb84ace
MuhammadUzain55 <uzainsheikh55@gmail.com>
2025-03-08 18:05:21 +0500

    Updated app.JS

--- app.js
+++ app.js
@@ -172,16 +172,21 @@
 // user enters “Karachi”, welcome the user like this:
 // “Welcome to city of lights”
 
-var cityName = prompt("city");
-if (cityName == "Karachi") {
-    alert("Welcome to city of lights")
-}
+// Ans: var cityName = prompt("city");
+// if (cityName == "Karachi") {
+//     alert("Welcome to city of lights")
+// }
 
 // Qno.2: Write a program to take “gender” as input from user. If the
 // user is male, give the message: Good Morning Sir. If the
 // user is female, give the message: Good Morning Ma’am.
 
-// Ans:
+// Ans: var gender = prompt("Gender");
+// if (gender == "male") {
+//     alert("Good Morning Sir!")
+// } else if (gender == "female"){
+//     alert("Good Morning Ma'am!")
+// }
 
 // Qno.3: Write a program to take input color of road traffic signal
 // from the user & show the message according to this table:
