3091c6b55cd6448b207d1436fa04089cf59e134a
MuhammadUzain55 <uzainsheikh55@gmail.com>
2025-03-24 23:02:42 +0500

    Updated app.js

--- app.js
+++ app.js
@@ -699,22 +699,3 @@ Ans: */
 //     console.log(stars);
 // }
 
-// function analyzeBudget(prices, items, budget) {
-//     let affordableItems = [];
-//     let totalCost = 0;
-//     let outOfBudgetCount = 0;
-
-//     for (let i = 0; i < prices.length; i++) {
-//         if (prices[i] <= budget) {
-//             affordableItems.push(items[i]);
-//             totalCost += prices[i];
-//         } else {
-//             outOfBudgetCount++;
-//         }
-//     }
-
-//     console.log(`Affordable items: ${affordableItems.join(", ") || "None"}`);
-//     console.log(`Total budget needed: ${totalCost}`);
-//     console.log(`Items out of budget: ${outOfBudgetCount}`);
-// }
-
