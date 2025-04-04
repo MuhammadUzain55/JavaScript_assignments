9a1ebdb6f2483e53628f953a917c8fae40d0f768
MuhammadUzain55 <uzainsheikh55@gmail.com>
2025-04-04 18:39:27 +0500

    Create folder and add file

---
+++ JS_Assinment-02/app.js
@@ -0,0 +1,13 @@
+// ========================= Chapter# 5 (Math Expressions) =================================
+
+// Qno.1: Write a program that take two numbers & add them in a
+// new variable. Show the result in your browser.
+
+let num1 = 5;
+let num2 = 3;
+const total = num1 + num2;
+
+document.write(`<h1>Sum of ${num1} and ${num2} is ${total}</h1>`);
+
+// document.write("<h1>Yah! I can write HTML content through JavaScript</h1>")
+
