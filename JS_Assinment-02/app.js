73efd7fffc57fbfa31fac6a56bf2dca0bf75e812
MuhammadUzain55 <uzainsheikh55@gmail.com>
2025-04-04 19:34:49 +0500

    Updated

--- JS_Assinment-02/app.js
+++ JS_Assinment-02/app.js
@@ -1,13 +1,33 @@
 // ========================= Chapter# 5 (Math Expressions) =================================
 
-// Qno.1: Write a program that take two numbers & add them in a
-// new variable. Show the result in your browser.
+/* Qno.01: Write a program that take two numbers & add them in a
+new variable. Show the result in your browser.
 
+Ans: 01.
 let num1 = 5;
 let num2 = 3;
 const total = num1 + num2;
 
-document.write(`<h1>Sum of ${num1} and ${num2} is ${total}</h1>`);
+document.write(`<h1>Sum of ${num1} and ${num2} is ${total}</h1>`); */
 
-// document.write("<h1>Yah! I can write HTML content through JavaScript</h1>")
+/* Qno.02: Repeat task1 for subtraction, multiplication, division &
+modulus.
+
+Ans: 02.
+let num1 = 5;
+let num2 = 3;
+
+const subTotal = num1 - num2;
+document.write(`<h1>Subtraction of ${num1} and ${num2} is ${subTotal}</h1>`);
+
+const diviTotal = num1 / num2;
+document.write(`<h1>Division of ${num1} and ${num2} is ${diviTotal}</h1>`);
+
+const mulTotal = num1 * num2;
+document.write(`<h1>Multiplication of ${num1} and ${num2} is ${mulTotal}</h1>`);
+
+const modTotal = num1 % num2;
+document.write(`<h1>Modulus of ${num1} and ${num2} is ${modTotal}</h1>`); */
+
+
 
