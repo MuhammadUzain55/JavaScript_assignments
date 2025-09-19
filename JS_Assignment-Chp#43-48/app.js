dffc823bdddec1ec7d88c8a7651fb6b3ba75673b
MuhammadUzain55 <uzainsheikh55@gmail.com>
2025-09-19 17:53:03 +0500

    Updated

--- JS_Assignment-Chp#43-48/app.js
+++ JS_Assignment-Chp#43-48/app.js
@@ -1,7 +1,16 @@
-let allImages = document.querySelectorAll("img");
+// Quistion No1: Display some Mobile images in browser. On click on an
+// image Show the message in alert to user.
 
-allImages.forEach((img) => {
-    img.addEventListener("click", () => {
-        alert("Thanks for purchasing a phone from us.");
-    });
-});
+// let allImages = document.querySelectorAll("img");
+
+// allImages.forEach((img) => {
+//     img.addEventListener("click", () => {
+//         alert("Thanks for purchasing a phone from us.");
+//     });
+// });
+
+// Quistion No.2: Display 10 student records in table and each row should contain a delete button. If you click on a button to delete a record, entire row should be deleted.
+
+let deleteBtn = document.querySelectorAll("button");
+
+
